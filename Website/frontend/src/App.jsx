da47733172b1db92ad2e4494443c3f61949fed01
Saurav Santosh Farkade <sauravfarkade9191@gmail.com>
2025-06-20 18:37:01 +0530

    ui fixed

--- Website/frontend/src/App.jsx
+++ Website/frontend/src/App.jsx
@@ -14,17 +14,20 @@ const App = () => {
 
     return (
         <div className="flex h-screen justify-around items-center bg-blue-300 border-2 border-red-500">
-            <div className="w-1/3 bg-blue-500 flex items-center justify-center h-full">
-                <Dashboard viewer={viewer} />
-            </div>
+            <div className='bg-yellow-500 w-1/4 h-full'>Sequence Manager </div>
             <div className='bg-red-500 w-full h-full flex flex-col justify-between'>
                 <div className='bg-green-500 h-full flex flex-col'>
-                    <div className='bg-yellow-500 h-1/4'>Viewer</div>
+                    <div className='bg-orange-500 h-2/4'>Viewer</div>
                     <div className='h-full'>
                         <ModelViewer onViewerInitialized={handleViewerInitialized} />
                     </div>
+
+                </div>
+                <div className=" bg-blue-500 flex items-center justify-center h-full">
+                    <Dashboard viewer={viewer} />
                 </div>
             </div>
+
         </div>
     );
 };
